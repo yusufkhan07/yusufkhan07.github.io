9ad9897d2f2e1b161d68a925c4203898271c6c85
Yousuf Khan <yusufkhanjee@gmail.com>
2020-12-13 10:17:51 +0500

    chore(app.js): remove react link

--- src/App.js
+++ src/App.js
@@ -9,14 +9,6 @@ function App() {
         <p>
           Hello World, I am Yousuf Khan!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
